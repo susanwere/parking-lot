969f99550282475bdbf53409171f299ae4a01f61
susanwere <blue.were@yahoo.com>
2020-03-21 17:21:47 +0300

    Create a ticket API - calculates new barcodes for each ticket - saves time when the ticket is given out - sets up tests with rspec

---
+++ app/controllers/api/tickets_controller.rb
@@ -0,0 +1,58 @@
+class Api::TicketsController < ApplicationController
+  require 'securerandom'
+  before_action :set_ticket, only: [:show, :update, :destroy]
+
+  # GET /tickets
+  def index
+    @tickets = Ticket.all
+
+    render json: @tickets
+  end
+
+  # GET /tickets/1
+  def show
+    render json: @ticket
+  end
+
+  # POST /tickets
+  def create
+    @ticket = Ticket.new(barcode: SecureRandom.hex(8), ticketedtime: Time.now())
+    
+
+    if @ticket.save
+      render json: @ticket, status: :created
+    else
+      render json: @ticket.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tickets/1
+  def update
+    if @ticket.update(ticket_params)
+      render json: @ticket
+    else
+      render json: @ticket.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tickets/1
+  def destroy
+    @ticket.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ticket
+      @ticket = Ticket.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: {
+        error: "Ticket with id #{params[:id]} not found",
+        status: :not_found
+      }
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def ticket_params
+      params.require(:ticket).permit(:barcode, :ticketedtime)
+    end
+end
